861235cbabf793513d1b774c8a442b7b698fd9e1
Rishit <rishpandey8097@gmail.com>
2024-01-16 01:49:45 +0530

    add: go support

--- lua/genzyy/plugins/lsp/lspconfig.lua
+++ lua/genzyy/plugins/lsp/lspconfig.lua
@@ -1,70 +1,92 @@
 return {
-  "neovim/nvim-lspconfig",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  config = function()
-    local lspconfig = require("lspconfig")
-    local lspui = require("lspconfig.ui.windows")
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
-    local capabilities = cmp_nvim_lsp.default_capabilities()
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	config = function()
+		local lspconfig = require("lspconfig")
+		local lspui = require("lspconfig.ui.windows")
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-    for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-    end
+		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		end
 
-    -- vim diagnostics
-    vim.diagnostic.config({
-      virtual_text = true,
-      signs = true,
-      underline = true,
-      update_in_insert = true,
-      severity_sort = true,
-    })
+		-- vim diagnostics
+		vim.diagnostic.config({
+			virtual_text = true,
+			signs = true,
+			underline = true,
+			update_in_insert = true,
+			severity_sort = true,
+		})
 
-    -- Auto Formatting
-    vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
+		-- Auto Formatting
+		vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
 
-    --LspInfo Borders
-    lspui.default_options.border = "double"
+		--LspInfo Borders
+		lspui.default_options.border = "double"
 
-    -- Managing language servers individually
-    -- pyright
-    lspconfig.pyright.setup({
-      capabilities = capabilities,
-    })
+		-- Managing language servers individually
+		-- pyright
+		lspconfig.pyright.setup({
+			capabilities = capabilities,
+		})
 
-    -- rust_analyzer
-    lspconfig.rust_analyzer.setup({
-      capabilities = capabilities,
-      -- Server-specific settings. See `:help lspconfig-setup`
-      settings = {
-        ["rust-analyzer"] = {},
-      },
-    })
+		-- rust_analyzer
+		lspconfig.rust_analyzer.setup({
+			capabilities = capabilities,
+			-- Server-specific settings. See `:help lspconfig-setup`
+			settings = {
+				["rust-analyzer"] = {},
+			},
+		})
 
-    -- clangd
-    lspconfig.clangd.setup({
-      capabilities = capabilities,
-      cmd = {
-        "clangd",
-        "--offset-encoding=utf-16",
-      },
-    })
+		-- clangd
+		lspconfig.clangd.setup({
+			capabilities = capabilities,
+			cmd = {
+				"clangd",
+				"--offset-encoding=utf-16",
+			},
+		})
 
-    -- Lua LS
-    lspconfig.lua_ls.setup({
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { "vim" },
-          },
-        },
-      },
-    })
-  end,
+		-- Lua LS
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+				},
+			},
+		})
+
+		-- golang
+		lspconfig.gopls.setup({
+			capabilities = capabilities,
+		})
+	end,
+	opts = {
+		settings = {
+			gopls = {
+				gofumpt = true,
+				codelenses = {
+					gc_details = false,
+					generate = true,
+					regenerate_cgo = true,
+					run_govulncheck = true,
+					test = true,
+					tidy = true,
+					upgrade_dependency = true,
+					vendor = true,
+				},
+			},
+		},
+	},
 }
--- lua/genzyy/plugins/treesitter.lua
+++ lua/genzyy/plugins/treesitter.lua
@@ -1,32 +1,36 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	name = "treesitter",
-	event = "bufWinEnter",
-	lazy = false,
-	init = function()
-		local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-		ts_update()
-	end,
-	config = function()
-		local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  name = "treesitter",
+  event = "bufWinEnter",
+  lazy = false,
+  init = function()
+    local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+    ts_update()
+  end,
+  config = function()
+    local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			ensure_installed = {
-				"c",
-				"lua",
-				"python",
-				"javascript",
-				"typescript",
-				"rust",
-				"vim",
-			},
-			sync_install = false,
-			auto_install = false,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-		})
-	end,
+    configs.setup({
+      ensure_installed = {
+        "c",
+        "lua",
+        "python",
+        "javascript",
+        "typescript",
+        "rust",
+        "vim",
+        "go",
+        "gomod",
+        "gowork",
+        "gosum",
+      },
+      sync_install = false,
+      auto_install = false,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+    })
+  end,
 }
