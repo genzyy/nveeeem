5385eb1ed33ef9f228e5886804b870c0ddfd5a8e
Rishit <rishpandey8097@gmail.com>
2024-01-26 02:07:03 +0530

    feat: autocmds

---
+++ lua/genzyy/core/autocmds.lua
@@ -0,0 +1,38 @@
+local function augroup(name)
+  return vim.api.nvim_create_augroup("genzyy_" .. name, { clear = true })
+end
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup("highlight_yank"),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup("last_loc"),
+  callback = function(event)
+    local exclude = { "gitcommit" }
+    local buf = event.buf
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].lazyvim_last_loc then
+      return
+    end
+    vim.b[buf].lazyvim_last_loc = true
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(buf)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- Fix conceallevel for json files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = augroup("json_conceal"),
+  pattern = { "json", "jsonc", "json5" },
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
--- lua/genzyy/core/init.lua
+++ lua/genzyy/core/init.lua
@@ -1,3 +1,4 @@
 require("genzyy.core.remaps")
 require("genzyy.core.options")
 require("genzyy.core.lazy")
+require("genzyy.core.autocmds")
