41fe27b12696c26b68c0f731b3bdff1e5737aa0b
Rishit <rishpandey8097@gmail.com>
2024-04-14 00:04:35 +0530

    add: catppuccin colourscheme

--- lua/genzyy/plugins/colorscheme.lua
+++ lua/genzyy/plugins/colorscheme.lua
@@ -1,7 +1,35 @@
 return {
-  "nyoom-engineering/oxocarbon.nvim",
-  name = "oxocarbon",
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
   config = function()
-    vim.cmd([[colorscheme oxocarbon]])
+    require("catppuccin").setup({
+      flavour = "mocha",
+      background = {
+        dark = "mocha",
+      },
+      transparent_background = false,
+      dim_inactive = {
+        enabled = true,
+        shade = "dark",
+        percentage = 0.32,
+      },
+      styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+      },
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+      },
+    })
+    vim.cmd([[colorscheme catppuccin]])
   end,
 }
